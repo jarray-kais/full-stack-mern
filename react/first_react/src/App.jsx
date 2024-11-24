dca0fbf0d9df924fd8e5227d9f0bb79afdbeb277
jarray-kais <jarraykais1@gmail.com>
2024-11-24 17:27:09 +0100

    add first react app
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

---
+++ react/first_react/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello Dojo !</h1>
+    <p className='todo'>things I Need to do</p>
+    <ul className='list'>
+      <li>Learn react</li>
+      <li>Climb mt.Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+
+    </>
+  )
+}
+
+export default App
