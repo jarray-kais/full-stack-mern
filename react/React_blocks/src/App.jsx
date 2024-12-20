d0f79bb9d2ef1e36488b1e24ac0dc0a7dd2cd71b
jarray-kais <jarraykais1@gmail.com>
2024-12-20 15:41:27 +0100

    update
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- react/React_blocks/src/App.jsx
+++ react/React_blocks/src/App.jsx
@@ -1,20 +1,16 @@
-import './App.css'
-import Header from './componenets/Header'
-import MAincontent from './componenets/MAincontent'
-import Navigation from './componenets/Navigation'
+import "./App.css";
+import Header from "./componenets/Header";
+import MAincontent from "./componenets/MAincontent";
+import Navigation from "./componenets/Navigation";
 
 function App() {
-
-
   return (
-    <>
-    <div className='app'>  
+    <div className="app">
       <Header />
       <Navigation />
       <MAincontent />
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
